f6beacf3510bff61159bfbba4b9c9a72501b30ec
mishiijp <maoni1jp@gmail.com>
2016-09-25 08:19:04 +0900

    csvread updated

--- csvreader.js
+++ csvreader.js
@@ -1,18 +1,21 @@
 var CSV = require('csv-lite');
 var fs = require('fs');
-var Converter = require('csvtojson').Converter;
-var converter = new Converter({});
+
 var async = require('async');
 var formatter = require('dateformat');
 
 module.exports = {
 
     readSteps: function(req,res) {
+        var Converter = require('csvtojson').Converter;
+        var converter = new Converter({});
         var csvPath = './csv/historical_data.csv';
         var resultArray = [];
+        console.log('aaaaaa');
         converter.on("end_parsed", function (jsonArray) {
             //歩数が同じ時分秒は無視する
             var stepcount = 0;
+            console.log('bbbbb');
             for(i = 0; i < jsonArray.length; i++) {
 
                 if (stepcount != jsonArray[i].st) {
@@ -24,10 +27,13 @@ module.exports = {
                     continue;
                 }
             }
+            console.log('ccccc');
             res.json(JSON.parse(JSON.stringify(resultArray)));
-
         });
-        fs.createReadStream(csvPath).pipe(converter);
+        var reader = fs.createReadStream(csvPath);
+        console.log(reader);
+        reader.pipe(converter);
+        console.log('dddddd');
     }
 
 };
