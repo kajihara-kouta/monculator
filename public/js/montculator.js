71eaf30d7ff477ff60bd2b99e7f7b7f0847eda67
mishiijp <maoni1jp@gmail.com>
2016-11-05 02:16:26 +0900

    一覧にユーザID追加

--- public/js/montculator.js
+++ public/js/montculator.js
@@ -16,6 +16,13 @@ app.controller('planCtrl', function($scope, $http, $state) {
         method: 'GET',
         url:'/apis/plan/get',
     }).success(function(data, status, headers, config){
+        for (i in data) {
+            var tmpfromdate = data[i].fromdate.substring(0,10);
+            data[i].fromdate = tmpfromdate;
+            var tmptodate = data[i].todate.substring(0,10);
+            data[i].todate = tmptodate;
+        }
+
         $scope.plans = data;
     })
 });
