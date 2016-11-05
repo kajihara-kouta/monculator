93e151d55b3c43c414b81527d630f03cf6a53ade
mishiijp <maoni1jp@gmail.com>
2016-11-06 03:34:53 +0900

    管理画面修正

--- public/js/montculator.js
+++ public/js/montculator.js
@@ -11,7 +11,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
 });
 
-app.controller('planCtrl', function($scope, $http, $state) {
+app.controller('topCtrl', function($scope, $http, $state) {
+    var vm = {};
+    vm['paneltitle'] = 'ダッシュボード';
+    $scope.vm = vm;
+});
+
+app.controller('planCtrl', function($rootScope, $scope, $http, $state) {
+    $scope.$parent.vm.paneltitle = '登山計画書一覧';
     $http({
         method: 'GET',
         url: '/apis/mountain/get',
@@ -33,4 +40,26 @@ app.controller('planCtrl', function($scope, $http, $state) {
 
         $scope.plans = data;
     })
+});
+
+app.controller('userCtrl', function($scope, $http, $state) {
+    $scope.$parent.vm.paneltitle = 'ユーザー情報一覧';
+    $http({
+        method:'GET',
+        url:'/apis/users/get',
+    }).success(function(data,status, headers, config) {
+        for (i in data) {
+            var sex = data[i].sex;
+            var sexViewVal;
+            if (sex == 'Male') {
+                sexViewVal = '男性';
+            } else if (sex == 'Female') {
+                sexViewVal = '女性';
+            } else {
+                sexViewVal = '';
+            }
+            data[i]['sex'] = sexViewVal;
+        }
+        $scope.users = data;
+    });
 });
