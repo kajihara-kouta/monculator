f2a054328539ce828e26c0284bf7af555576d16d
mishiijp <maoni1jp@gmail.com>
2016-11-06 05:45:01 +0900

    ユーザー一覧及びモーダル対応

--- public/js/montculator.js
+++ public/js/montculator.js
@@ -17,7 +17,7 @@ app.controller('topCtrl', function($scope, $http, $state) {
     $scope.vm = vm;
 });
 
-app.controller('planCtrl', function($rootScope, $scope, $http, $state) {
+app.controller('planCtrl', function($scope, $http, $state) {
     $scope.$parent.vm.paneltitle = '登山計画書一覧';
     $http({
         method: 'GET',
@@ -42,7 +42,7 @@ app.controller('planCtrl', function($rootScope, $scope, $http, $state) {
     })
 });
 
-app.controller('userCtrl', function($scope, $http, $state) {
+app.controller('userCtrl', function($scope, $http, $state, $uibModal) {
     $scope.$parent.vm.paneltitle = 'ユーザー情報一覧';
     $http({
         method:'GET',
@@ -61,5 +61,64 @@ app.controller('userCtrl', function($scope, $http, $state) {
             data[i]['sex'] = sexViewVal;
         }
         $scope.users = data;
+    });
+    $scope.showModal = function(index) {
+        console.log(index);
+        console.log($scope.users[index]);
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/usermodal.html',
+            controller: 'userModalCtrl',
+            scope:$scope,
+            resolve: {
+                params: function() {
+                    return {
+                        _id: $scope.users[index]._id,
+                        userid: $scope.users[index].userid,
+                        name: $scope.users[index].name,
+                        postalcd: $scope.users[index].postalcd,
+                        sex: $scope.users[index].sex,
+                        birthymd: $scope.users[index].birthymd,
+                        address: $scope.users[index].address,
+                        tel: $scope.users[index].tel,
+                        bloodtype: $scope.users[index].bloodtype,
+                        rhtype: $scope.users[index].rhtype
+                    }
+                }
+            }
+        });
+        modalInstance.result.then(
+            function(result) {
+                console.log(result);
+//                $http({
+//                    method::'GET',
+//                    url:'/apis/users/get/'
+//                })
+            }
+        );
+
+    };
+});
+
+app.controller('userModalCtrl', function($scope, $http, $uibModalInstance, params) {
+
+    $scope.userid = params.userid;
+    $scope.name = params.name;
+    $scope.postalcd = params.postalcd;
+    $scope.sex = params.sex;
+    //生年月日を表示用に修正
+    var viewYmd = params.birthymd.substring(0,4) + '年' + params.birthymd.substring(4,6) + '月' + params.birthymd.substring(6,8) + '日';
+    $scope.birthymd = viewYmd;
+    $scope.address = params.address;
+    $scope.tel = params.tel;
+    $scope.bloodtype = params.bloodtype + '型';
+    $scope.rhtype = (params.rhtype == 'Positive')? '+' : '-';
+    $scope.closeBtn = function() {
+        $uibModalInstance.close('done');
+    }
+    $http({
+        method:'GET',
+        url:'/apis/emergencyContact/get/' + params.userid,
+    }).success(function(data, status, headers, config) {
+        $scope.emergencyContact = data;
     });
 });
